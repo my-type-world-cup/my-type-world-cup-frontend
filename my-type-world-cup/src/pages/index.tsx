7fe26e1efc621ae2b7cae4b41c0bcdb0d6006cee
nakjun12 <wns450@gmail.com>
2023-04-20 13:59:39 +0900

    feat: card 완성

--- my-type-world-cup/src/pages/index.tsx
+++ my-type-world-cup/src/pages/index.tsx
@@ -1,17 +1,17 @@
-import Cards from "@/components/main/Cards";
+import Card from "@/components/main/Card";
 import SearchBar from "@/components/main/SearchBar";
 import SortButtons from "@/components/main/SortButtons";
 import { useState } from "react";
 export default function Home() {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   return (
-    <main className="flex h-screen flex-col bg-blue pt-14">
+    <main className="flex h-screen flex-col pt-14">
       <SearchBar isOpen={isOpen} setIsOpen={setIsOpen} />
       <div className="mt-12 mx-auto">
         <SortButtons />
       </div>
       <article className="w-full">
-        <Cards />
+        <Card />
       </article>
     </main>
   );
