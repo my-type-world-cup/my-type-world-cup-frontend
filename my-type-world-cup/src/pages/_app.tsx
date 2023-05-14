4054cc528338b0fe5ba5f25e55acc99a61a03ca8
nakjun12 <wns450@gmail.com>
2023-05-15 01:19:47 +0900

    Feat #4 : cache-header 추가

--- my-type-world-cup/src/pages/_app.tsx
+++ my-type-world-cup/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import Header from "@/components/all/Header";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { SWRConfig } from "swr";
 
 function App({ Component, pageProps }: AppProps) {
   return (
@@ -25,7 +26,9 @@ function App({ Component, pageProps }: AppProps) {
       <div className="bg-white max-w-[480px] w-full sm:min-w-[480px] mx-auto sm:mx-20  shadow-lg h-5/6">
         <Header />
         <div className="-mt-[63px]">
-          <Component {...pageProps} />
+          <SWRConfig>
+            <Component {...pageProps} />
+          </SWRConfig>
         </div>
       </div>
     </div>
