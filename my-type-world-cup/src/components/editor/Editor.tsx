b2ec3157dafd625cf4a97a7e8ec4d632171e8eec
nakjun12 <wns450@gmail.com>
2023-05-20 15:47:19 +0900

    Style #1 : editor 페이지 디자인 수정

--- my-type-world-cup/src/components/editor/Editor.tsx
+++ my-type-world-cup/src/components/editor/Editor.tsx
@@ -1,48 +1,81 @@
-import { ChangeEvent, FormEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
+import { postWorldcup } from "../../lib/atom/atom";
+import type { Post_worldcup } from "../../type/Types";
+interface EditorProps {}
 
-interface EditorProps {
-  onSave: (title: string, description: string, isPublic: boolean) => void;
-}
-
-const Editor = ({ onSave }: EditorProps) => {
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-  const [isPublic, setIsPublic] = useState(false);
-
+const Editor = ({}: EditorProps) => {
+  const [title, setTitle] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+  const [isPublic, setIsPublic] = useState<boolean>(false);
+  const [password, setPassword] = useState<string | null>(null);
+  const [worldcup, setWorldcup] = useRecoilState<Post_worldcup | null>(
+    postWorldcup
+  );
+  useEffect(() => {
+    setPassword(null);
+  }, [isPublic]);
+  console.log(worldcup, "worldcup");
   const handleSave = () => {
-    onSave(title, description, isPublic);
+    setWorldcup((el) => ({
+      ...el!,
+      title: title,
+      description: description,
+      password: password ? password : null,
+    }));
   };
 
   const handlePublicChange = (event: ChangeEvent<HTMLInputElement>) => {
     setIsPublic(event.target.value === "public");
   };
 
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    handleSave();
+  const handleReset = () => {
+    setTitle("");
+    setDescription("");
+    setPassword(null);
   };
 
   return (
-    <form className="mt-8 mx-8 text-lg flex flex-col" onSubmit={handleSubmit}>
+    <div
+      className="sm:pt-20 mt-4 sm:mt-8 mx-8 text-lg flex flex-col -pb-20 h-screen"
+      // onSubmit={handleSubmit}
+    >
       <label htmlFor="title">제목</label>
       <input
         type="text"
-        className="border-b-[1px] border-main text-gray mt-1"
+        className="border-b-[1px] border-main text-gray mt-1 pl-4"
         placeholder="이상형 월드컵의 제목을 입력해주세요"
         value={title}
         onChange={(event) => setTitle(event.target.value)}
       />
       <br />
 
-      <label htmlFor="description">설명</label>
+      <label htmlFor="description" className="pt-8">
+        설명
+      </label>
       <textarea
-        className="border-[1px] border-main mt-1"
+        className="border-[1px] border-main mt-1 outline-none"
         value={description}
         onChange={(event) => setDescription(event.target.value)}
+        rows={3}
       />
       <br />
 
-      <div className="flex justify-around items-center">
+      {!isPublic && (
+        <div className="flex justify-center flex-col pt-4">
+          <label htmlFor="password">비밀번호</label>
+          <input
+            type="password"
+            className="border-[1px] border-main text-gray mt-2 w-auto p-1 pl-4"
+            maxLength={4}
+            disabled={isPublic}
+            placeholder="비밀번호를 입력해주세요"
+            value={password ? password : ""}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </div>
+      )}
+      <div className="flex justify-around items-center mt-6">
         <label className="flex items-center">
           <input
             type="radio"
@@ -55,7 +88,7 @@ const Editor = ({ onSave }: EditorProps) => {
           공개
         </label>
 
-        <label className="flex items-center ">
+        <label className="flex items-center">
           <input
             type="radio"
             name="public"
@@ -67,9 +100,21 @@ const Editor = ({ onSave }: EditorProps) => {
           비공개
         </label>
       </div>
-
-      <button type="submit">저장</button>
-    </form>
+      <button
+        type="button"
+        onClick={() => handleSave()}
+        className="mt-20 px-4 py-2 bg-main text-white rounded-md hover:scale-105 transition-all"
+      >
+        저장
+      </button>
+      <button
+        type="button"
+        onClick={handleReset}
+        className="mt-4 sm:mt-8 px-4 py-2 bg-main text-white rounded-md hover:scale-105 transition-all"
+      >
+        초기화
+      </button>
+    </div>
   );
 };
 export default Editor;
--- my-type-world-cup/src/pages/editors/index.tsx
+++ my-type-world-cup/src/pages/editors/index.tsx
@@ -2,16 +2,31 @@ import Editor from "@/components/editor/Editor";
 import ImageUpload from "@/components/editor/ImageUpload";
 import type { Step } from "@/components/editor/TabButton";
 import TabButtons from "@/components/editor/TabButton";
+import { postWorldcup } from "@/lib/atom/atom";
+import { Post_worldcup } from "@/type/Types";
 import { useState } from "react";
+import { useRecoilState } from "recoil";
 const WorldCupEditor = () => {
-  const [isWord, setIsWord] = useState<Step>("first");
-  const onSave = (title: string, description: string, isPublic: boolean) => {};
+  const [worldcup, setWorldcup] = useRecoilState<Post_worldcup | null>(
+    postWorldcup
+  );
+  const [isWord, setIsWord] = useState<Step>("1");
 
+  const content = (() => {
+    switch (isWord) {
+      case "1":
+        return <Editor />;
+      case "2":
+        return <ImageUpload />;
+      default:
+        return null;
+    }
+  })();
   return (
     <div className="pt-12">
       <TabButtons isWord={isWord} setIsWord={setIsWord} />
-      <Editor onSave={onSave} />
-      <ImageUpload />
+
+      {content}
     </div>
   );
 };
