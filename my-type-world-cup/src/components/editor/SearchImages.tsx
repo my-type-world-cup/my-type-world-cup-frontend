b08aedfde66f9f960bd90fe5351a2f0bfd428208
nakjun12 <wns450@gmail.com>
2023-05-21 13:37:07 +0900

    Feat #1 : image 업로드 리스트 추가

--- my-type-world-cup/src/components/editor/SearchImages.tsx
+++ my-type-world-cup/src/components/editor/SearchImages.tsx
@@ -1,28 +1,51 @@
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 interface ImageListProps {
   data: string[] | null;
+  setSize: (size: ((prevSize: number) => number) | number) => void;
 }
 
-const SearchImages: React.FC<ImageListProps> = ({ data }) => {
+const SearchImages: React.FC<ImageListProps> = ({ data, setSize }) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const handleScroll = () => {
+      const { scrollLeft, scrollWidth, clientWidth } = containerRef.current!;
+      if (scrollLeft + clientWidth >= scrollWidth) {
+        setSize((el) => el + 1);
+      }
+    };
+    const container = containerRef.current!;
+    container.addEventListener("scroll", handleScroll);
+
+    return () => {
+      return container.removeEventListener("scroll", handleScroll);
+    };
+  }, [setSize]);
   return (
-    <div className="overflow-x-scroll bg-white border-main border-2">
+    <div
+      className={
+        data && data.length > 0
+          ? "overflow-scroll bg-white px-2 py-1"
+          : " bg-white border border-gray rounded px-2 py-1"
+      }
+      ref={containerRef}
+    >
       <div className="flex w-fit bg-white">
         {data && data.length > 0 ? (
           data.map((imageUrl, index) => (
-            <div key={index} className="w-52 h-80">
+            <div key={index} className="w-32 h-60 flex items-center mr-2">
               <Image
                 src={imageUrl}
                 alt={`Image ${index}`}
                 width={200}
                 height={200}
-                className="mr-1"
+                className="cursor-pointer"
               />
             </div>
           ))
         ) : (
-          <div>검색 결과가 없습니다.</div>
+          <div className="bg-white">검색 결과가 없습니다.</div>
         )}
       </div>
     </div>
