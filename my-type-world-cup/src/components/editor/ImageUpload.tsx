493c4e31fc8dc0a0f03f8df2b05597e9050385c7
nakjun12 <wns450@gmail.com>
2023-05-21 13:37:57 +0900

    Feat #1 :  x축 인피니티 스크롤 구현

--- my-type-world-cup/src/components/editor/ImageUpload.tsx
+++ my-type-world-cup/src/components/editor/ImageUpload.tsx
@@ -1,9 +1,9 @@
+import { fetcher } from "@/lib/Helper";
 import { BACK_URL } from "@/lib/config";
 import type { Search_Image } from "@/type/Types";
-import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import useSWRInfinite from "swr/infinite";
 import SearchBar from "../main/SearchBar";
-import DragImage from "./DragImage";
 import ImageEditor from "./ImageEditor";
 import SearchImage from "./SearchImages";
 type Props = {};
@@ -12,68 +12,40 @@ export default function ImageUpload({}: Props) {
   const [search, setSearch] = useState<string>("");
   const [images, setImages] = useState<Search_Image | null>(null);
   const [page, setPage] = useState<number>(1);
-  useEffect(() => {
-    console.log(search.trim(), "결과");
-    if (!search.trim()) return;
-    const keyword = search.slice(1);
-    console.log(search.trim(), "결과1");
-    fetch(`${BACK_URL}/images?${keyword}&page=${page}&size=10`)
-      .then((res) => res.json())
-      .then((res) => setImages(res));
-  }, [search, page]);
-  console.log(images, "이미지");
+  const keyword = search.slice(1);
+  const { data, mutate, size, setSize, isValidating, isLoading } =
+    useSWRInfinite<Search_Image>((index) => {
+      // keyword가 비어 있으면 빈 문자열 반환
+      if (!keyword) {
+        return "";
+      }
+      return `${BACK_URL}/images?${keyword}&page=${index + 1}&size=20`;
+    }, fetcher);
+  const searchData: string[] = data ? data.map((v) => v.data).flat() : [];
+  // useEffect(() => {
+  //   console.log(search.trim(), "결과");
+  //   if (!search.trim()) return;
+  //   const keyword = search.slice(1);
+  //   console.log(search.trim(), "결과1");
+  //   fetch(`${BACK_URL}/images?${keyword}&page=${page}&size=20`)
+  //     .then((res) => res.json())
+  //     .then((res) => setImages(res));
+  // }, [search, page]);
+
+  console.log(data, images, "이미지");
+
   return (
     <section className="mt-12 flex flex-col mx-8 text-lg">
       <div>
         <label className=""> 이미지 업로드</label>
-        <DragImage />
+        <SearchImage data={searchData ? searchData : null} setSize={setSize} />
       </div>
 
       <SearchBar setSearch={setSearch} />
       <div className="flex justify-between">
         <h1 className="text-xl mb-4">Image List</h1>
-        <div className="flex">
-          <button
-            type="button"
-            className="input-button"
-            onClick={() =>
-              setPage((prev) => {
-                if (prev === images?.pageInfo.totalPages) return prev;
-                return prev + 1;
-              })
-            }
-          >
-            <Image
-              src="/icon/grayPlus.svg"
-              alt="plus"
-              className="cursor-pointer"
-              width={20}
-              height={18}
-              priority
-            />
-          </button>
-          <button
-            type="button"
-            className="input-button ml-2"
-            onClick={() =>
-              setPage((prev) => {
-                if (prev === 1) return prev;
-                return prev - 1;
-              })
-            }
-          >
-            <Image
-              src="/icon/grayMinus.svg"
-              alt="minus"
-              className="cursor-pointer"
-              width={20}
-              height={18}
-              priority
-            />
-          </button>
-        </div>
       </div>
-      <SearchImage data={images ? images.data : null} />
+      <SearchImage data={searchData ? searchData : null} setSize={setSize} />
 
       <ImageEditor />
     </section>
