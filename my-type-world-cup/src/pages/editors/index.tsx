ceb9afccb8bff611b97d013cfd63888df260c29e
nakjun12 <wns450@gmail.com>
2023-05-24 22:36:47 +0900

    Feat #8 : 토큰 확인 및 Recoil 데이터 추가

--- my-type-world-cup/src/pages/editors/index.tsx
+++ my-type-world-cup/src/pages/editors/index.tsx
@@ -2,28 +2,35 @@ import Editor from "@/components/editor/Editor";
 import ImageUpload from "@/components/editor/ImageUpload";
 import type { Step } from "@/components/editor/TabButton";
 import TabButtons from "@/components/editor/TabButton";
-import { postWorldcup } from "@/lib/atom/atom";
-import { Post_worldcup } from "@/type/Types";
-import { useState } from "react";
-import { useRecoilState } from "recoil";
+import { accessTokenState, postWorldcup } from "@/lib/atom/atom";
+import { Post_res } from "@/type/Types";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { useRecoilValue } from "recoil";
 const WorldCupEditor = () => {
-  const [worldcup, setWorldcup] = useRecoilState<Post_worldcup | null>(
-    postWorldcup
-  );
+  const saveWorldcup = useRecoilValue<Post_res | null>(postWorldcup);
+  const accessToken = useRecoilValue<string | null>(accessTokenState);
   const [isWord, setIsWord] = useState<Step>("1");
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!accessToken) {
+      router.push("/"); // 홈으로 이동합니다.
+    }
+  }, [accessToken, router]);
 
   const content = (() => {
     switch (isWord) {
       case "1":
         return <Editor />;
       case "2":
-        return <ImageUpload />;
+        return <ImageUpload saveWorldcup={saveWorldcup} />;
       default:
         return null;
     }
   })();
   return (
-    <div className="pt-12">
+    <div className="pt-12 relative">
       <TabButtons isWord={isWord} setIsWord={setIsWord} />
 
       {content}
