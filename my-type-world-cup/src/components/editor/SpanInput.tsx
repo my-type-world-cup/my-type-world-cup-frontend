70567b4dce6e451c9274a0efb9031115f5923d9b
nakjun12 <wns450@gmail.com>
2023-05-28 23:54:23 +0900

    Feat : 이름 수정 및 취소 기능 추가

---
+++ my-type-world-cup/src/components/editor/SpanInput.tsx
@@ -0,0 +1,112 @@
+import { saveWorldcups } from "@/lib/atom/atom";
+import type { Save_data } from "@/type/Types";
+import { useEffect, useRef, useState } from "react";
+import { useSetRecoilState } from "recoil";
+const SpanInput = ({
+  name,
+  accessToken,
+  item,
+}: {
+  name: string;
+  accessToken: string | null;
+  item: Save_data;
+}) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [text, setText] = useState(name);
+  const [savedText, setSavedText] = useState(name);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const blurRef = useRef<HTMLDivElement>(null);
+  const setSaveList = useSetRecoilState(saveWorldcups);
+  useEffect(() => {
+    const handleClickOutside = (event: any) => {
+      if (blurRef.current && !blurRef.current.contains(event.target)) {
+        setIsEditing(false);
+        setText(savedText);
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [blurRef, setIsEditing, setText, savedText]);
+  useEffect(() => {
+    if (isEditing && inputRef.current) {
+      inputRef.current.focus();
+    }
+    // if (!isEditing && !value) setValue(save);
+    // if (value !== name && accessToken && !isEditing) {
+    //   console.log("발동");
+    //   patch_candidates(accessToken, { ...item, name: value });
+    //   setSaveList((prev) => {
+    //     const newSave = prev.map((list: Save_data) => {
+    //       if (list.id === item.id) {
+    //         return { ...list, name: value };
+    //       }
+    //       return list;
+    //     });
+    //     return newSave;
+    //   });
+    // }
+  }, [isEditing]);
+
+  const handleClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleSave = () => {
+    setSavedText(text);
+    setIsEditing(false);
+  };
+
+  const handleCancel = () => {
+    setIsEditing(false);
+    setText(savedText);
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+
+  return (
+    <div className="">
+      {isEditing ? (
+        <div ref={blurRef}>
+          <input
+            type="text"
+            ref={inputRef}
+            value={text}
+            onChange={handleChange}
+            spellCheck={false}
+            className="w-full border border-gray p-2"
+          />
+          <div className="flex justify-end space-x-4 p-2">
+            <span className="cursor-pointer" onClick={handleSave}>
+              저장
+            </span>
+            <span className="cursor-pointer" onClick={handleCancel}>
+              취소
+            </span>
+          </div>
+        </div>
+      ) : (
+        <>
+          <div
+            className="text-gray hover:text-main w-full p-2 border"
+            onClick={handleClick}
+          >
+            {savedText}
+          </div>
+          <div className="flex justify-end space-x-4 p-2">
+            <span className="cursor-pointer" onClick={handleClick}>
+              수정
+            </span>
+            <span className="cursor-pointer">삭제</span>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default SpanInput;
