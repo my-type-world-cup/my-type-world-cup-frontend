f0de59475176d3a0715b39cb28179209f6f3bb69
nakjun12 <wns450@gmail.com>
2023-06-19 21:49:38 +0900

    remove : 여백 삭제

--- my-type-world-cup/src/components/game/InGameModal.tsx
+++ my-type-world-cup/src/components/game/InGameModal.tsx
@@ -114,7 +114,7 @@ export default function Modal({
         </>
       ) : (
         <div className="mx-4">
-          현재 인원이{" "}
+          현재 인원이
           <span className="text-error">{data.candidatesCount}명</span>
           이어서 게임을 시작할 수 없습니다 후보를 등록해주세요
         </div>
