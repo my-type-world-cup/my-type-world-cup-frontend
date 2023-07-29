2a540743293bafb7eafdd8743959f5ae7a8f44f1
nakjun12 <wns450@gmail.com>
2023-07-29 23:38:07 +0900

    Fix #15 : og 데이터 수정

--- my-type-world-cup/src/pages/_app.tsx
+++ my-type-world-cup/src/pages/_app.tsx
@@ -10,20 +10,31 @@ function App({ Component, pageProps }: AppProps) {
     <div className="bg-hover flex justify-center items-center mt-[-1px] w-auto overflow-hidden">
       <Head>
         <title>이상형 월드컵</title>
-        <meta name="description" content="당신만의 이상형을 찾아주세요!." />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/icon/whiteDolphin2.svg" />
         <meta property="og:title" content="이상형 월드컵" />
         <meta
-          property="og:description"
-          content="모바일로 즐기는 이상형월드컵을 찾아주세요!"
+          itemProp="image"
+          content="https://velog.velcdn.com/images/wns450/post/57bab195-0f49-4b08-889f-968dc39853d8/image.png"
         />
+        <meta name="description" content="당신만의 이상형을 찾아주세요!." />
 
-        <meta property="og:image" content="/icon/whiteDolphin2.svg" />
-        <link rel="icon" href="/icon/whiteDolphin2.svg" />
         <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no"
+          property="og:description"
+          content="당신의 이상형을 찾는 이상형 월드컵 사이트입니다!"
         />
+        <meta property="og:site_name" content="Dolpick" />
+        <meta
+          property="og:image"
+          content="https://velog.velcdn.com/images/wns450/post/75b80a2a-510a-49b4-a6cb-91a048ff53d5/image.jpeg"
+        />
+        <meta
+          itemProp="image"
+          content="https://velog.velcdn.com/images/wns450/post/57bab195-0f49-4b08-889f-968dc39853d8/image.png"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://dolpick.com" />
+
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <div className="my-auto h-0 lg:h-auto mt-40 mr-4 hidden lg:block">
         <p className="mt-4 text-2xl text-left">나의 마음을 확인하세요</p>
