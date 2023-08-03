662144337dbd6cdce641c69420d53246a1372dc3
nakjun12 <wns450@gmail.com>
2023-08-03 23:58:20 +0900

    refactor : 함수 정리 및 주석 추가

--- my-type-world-cup/src/pages/index.tsx
+++ my-type-world-cup/src/pages/index.tsx
@@ -7,67 +7,67 @@ import { BACK_URL } from "@/lib/config";
 import { MainWorldcup, WorldcupsResponse } from "@/type/Types";
 import { useEffect, useRef, useState } from "react";
 import useSWRInfinite from "swr/infinite";
-const PAGE_SIZE = 10;
 
 export type Value = "playCount" | "createdAt" | "commentCount";
 
 export default function Home({}: {}) {
-  const containerRef = useRef<HTMLDivElement>(null);
-  const [isOpen, setIsOpen] = useState<boolean>(false); //메뉴바 관리
-  const [sort, setSort] = useState<Value>("playCount"); //정렬 관리
-  const [search, setSearch] = useState<string>(""); //검색 관리
-  console.log(sort);
+	const containerRef = useRef<HTMLDivElement>(null);
 
-  const { data, mutate, size, setSize, isValidating, isLoading } =
-    useSWRInfinite<WorldcupsResponse>(
-      (index) =>
-        `${BACK_URL}/worldcups?page=${index + 1}&size=10&sort=${sort}${search}`,
-      fetcher
-    );
+	const [sort, setSort] = useState<Value>("playCount"); //정렬 관리
+	const [search, setSearch] = useState<string>(""); //검색 관리
 
-  const isReachingEnd = data && data[data.length - 1]?.data.length < PAGE_SIZE;
-  useEffect(() => {
-    const handleScroll = () => {
-      const { scrollTop, scrollHeight, clientHeight } = containerRef.current!;
-      if (scrollTop + clientHeight >= scrollHeight && !isReachingEnd) {
-        setSize((el) => el + 1);
-        console.log("하이");
-      }
-    };
-    const container = containerRef.current!;
-    container.addEventListener("scroll", handleScroll);
+	const { data, setSize } = useSWRInfinite<WorldcupsResponse>(
+		(index) =>
+			`${BACK_URL}/worldcups?page=${
+				index + 1
+			}&size=10&sort=${sort}${search}`,
+		fetcher
+	);
 
-    return () => {
-      return container.removeEventListener("scroll", handleScroll);
-    };
-  }, [isReachingEnd, setSize]);
-  const worldcups: MainWorldcup[] = data ? data.map((v) => v.data).flat() : [];
+	// 이상형 월드컵 스크롤 끝 확인 변수
+	const isReachingEnd =
+		data && data[data.length - 1]?.data.length < 10;
 
-  //예시는 모두다 배열임
-  const isLoadingMore =
-    isLoading || (size > 0 && data && typeof data[size - 1] === "undefined"); //로딩중
+	useEffect(() => {
+		const handleScroll = () => {
+			const { scrollTop, scrollHeight, clientHeight } =
+				containerRef.current!;
 
-  // 더이상 없을때 체크
-  const isRefreshing = isValidating && data && data.length === size; // 요청중
+			if (
+				scrollTop + clientHeight >= scrollHeight &&
+				!isReachingEnd
+			) {
+				setSize((el) => el + 1);
+			}
+		};
+		const container = containerRef.current!;
+		container.addEventListener("scroll", handleScroll);
 
-  return (
-    <>
-      <main
-        className="flex h-screen flex-col overflow-y-scroll relative pt-24"
-        ref={containerRef}
-      >
-        <SearchBar setSearch={setSearch} />
-        <div className="mt-12 mx-auto">
-          <SortButtons setSort={setSort} sort={sort} />
-        </div>
-        <article className="w-full h-auto ">
-          {data
-            ? worldcups.map((v) => <Card key={v.id} worldcup={v} />)
-            : new Array(10).fill(1).map((_, i) => {
-                return <CardSkeleton key={i} />;
-              })}
-        </article>
-      </main>
-    </>
-  );
+		return () => {
+			return container.removeEventListener("scroll", handleScroll);
+		};
+	}, [isReachingEnd, setSize]);
+
+	// 관리하기 편한 값으로 수정
+	const worldcups: MainWorldcup[] = data
+		? data.map((v) => v.data).flat()
+		: [];
+
+	return (
+		<main
+			className='flex h-screen flex-col overflow-y-scroll relative pt-24'
+			ref={containerRef}>
+			<SearchBar setSearch={setSearch} />
+			<div className='mt-12 mx-auto'>
+				<SortButtons setSort={setSort} sort={sort} />
+			</div>
+			<article className='w-full h-auto '>
+				{data
+					? worldcups.map((v) => <Card key={v.id} worldcup={v} />)
+					: new Array(10).fill(1).map((_, i) => {
+							return <CardSkeleton key={i} />;
+					  })}
+			</article>
+		</main>
+	);
 }
