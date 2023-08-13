9ed5d7fdb94ca72235fdfaeb4d5151ef7e2baa48
nakjun12 <wns450@gmail.com>
2023-08-13 21:01:48 +0900

    fix : button tag 추가 및 이벤트 버블링으로 수정
    image 태그 onClick 제거 및 button 태그 추가 및 이벤트 버블링

--- my-type-world-cup/src/components/main/SearchBar.tsx
+++ my-type-world-cup/src/components/main/SearchBar.tsx
@@ -10,12 +10,7 @@ interface Props {
 const SearchBar = ({ setSearch }: Props) => {
 	const [searchTerm, setSearchTerm] = useState("");
 
-	const handleSearch = (
-		e:
-			| React.FormEvent<HTMLFormElement>
-			| React.MouseEvent<HTMLImageElement>
-	) => {
-		console.log("히");
+	const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
 		const trimmedSearchTerm = searchTerm.trim();
 
@@ -27,22 +22,23 @@ const SearchBar = ({ setSearch }: Props) => {
 	};
 
 	return (
-		<div className='flex items-center justify-center'>
+		<div className="flex items-center justify-center">
 			<form
-				className='bg-white w-72 h-10 relative flex items-center justify-center'
+				className="bg-white w-72 h-10 relative flex items-center justify-center"
 				onSubmit={handleSearch}>
-				<Image
-					src='/icon/search.svg'
-					alt='Search'
-					className='absolute right-4 pb-2 cursor-pointer'
-					width={25}
-					height={25}
-					onClick={handleSearch}
-				/>
+				<button type="submit" className="absolute right-4 pb-2">
+					<Image
+						src="/icon/search.svg"
+						alt="Search button"
+						width={25}
+						height={25}
+					/>
+				</button>
+
 				<input
-					type='text'
-					placeholder='당신의 이상형을 찾아주세요'
-					className='border-b-2 border-b-gray text-gray pb-2 pl-3 text-xl w-[280px] h-10 '
+					type="text"
+					placeholder="당신의 이상형을 찾아주세요"
+					className="border-b-2 border-b-gray text-gray pb-2 pl-3 text-xl w-[280px] h-10 "
 					onChange={(e) => setSearchTerm(e.target.value)}
 				/>
 			</form>
