fbadd888af47c6d5c20ab53ba9b58eb78bf53670
nakjun12 <wns450@gmail.com>
2023-08-14 03:56:34 +0900

    remove : 안쓰는 로직 제거

--- my-type-world-cup/src/pages/editors/index.tsx
+++ my-type-world-cup/src/pages/editors/index.tsx
@@ -9,41 +9,38 @@ import { useEffect, useState } from "react";
 import { useRecoilValue } from "recoil";
 
 const WorldCupEditor = () => {
-  const saveWorldcup = useRecoilValue<Post_res | null>(postWorldcup);
-  const accessToken = useRecoilValue<string | null>(accessTokenState);
-  const [isNumber, setIsNumber] = useState<Step>("1");
-  const router = useRouter();
+	const saveWorldcup = useRecoilValue<Post_res | null>(postWorldcup);
+	const accessToken = useRecoilValue<string | null>(accessTokenState);
+	const [isNumber, setIsNumber] = useState<Step>("1");
+	const router = useRouter();
 
-  useEffect(() => {
-    if (!accessToken) {
-      router.push("/"); // 홈으로 이동합니다.
-    }
-  }, [accessToken, router]);
+	useEffect(() => {
+		if (!accessToken) {
+			router.push("/"); // 홈으로 이동합니다.
+		}
+	}, [accessToken, router]);
 
-  const content = (() => {
-    switch (isNumber) {
-      case "1":
-        return <Editor setIsNumber={setIsNumber} />;
-      case "2":
-        return (
-          <ImageUpload
-            accessToken={accessToken}
-            saveWorldcup={saveWorldcup}
-            setIsNumber={setIsNumber}
-          />
-        );
+	const content = (() => {
+		switch (isNumber) {
+			case "1":
+				return <Editor setIsNumber={setIsNumber} />;
+			case "2":
+				return (
+					<ImageUpload
+						accessToken={accessToken}
+						saveWorldcup={saveWorldcup}
+						setIsNumber={setIsNumber}
+					/>
+				);
+		}
+	})();
+	return (
+		<div className="pt-12 relative">
+			<TabButtons isNumber={isNumber} setIsNumber={setIsNumber} />
 
-      default:
-        return null;
-    }
-  })();
-  return (
-    <div className="pt-12 relative">
-      <TabButtons isNumber={isNumber} setIsNumber={setIsNumber} />
-
-      {content}
-    </div>
-  );
+			{content}
+		</div>
+	);
 };
 
 export default WorldCupEditor;
