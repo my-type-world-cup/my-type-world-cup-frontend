f53a6202bfcbf3e2613e77d8c29909557e7aed18
nakjun12 <wns450@gmail.com>
2023-08-15 23:02:16 +0900

    rename : 대소문자 오류 수정

--- my-type-world-cup/src/components/all/GameSet.tsx
+++ my-type-world-cup/src/components/all/GameSet.tsx
@@ -7,28 +7,28 @@ import CommentList from "../game/CommentList";
 import GameButtons from "../main/GameButtons";
 
 type Props = {
-  id: number;
-  winner?: Contestant;
+	id: number;
+	winner?: Contestant;
 };
 
 export default function GameSet({ id, winner }: Props) {
-  const [rendering, setRendering] = useState<boolean>(false);
-  const accessToken = useRecoilValue(accessTokenState);
-  // console.log(accessToken);
+	const [rendering, setRendering] = useState<boolean>(false);
+	const accessToken = useRecoilValue(accessTokenState);
+	// console.log(accessToken);
 
-  return (
-    <>
-      <GameButtons isreload={true} id={id} />
-      <Comment
-        winner={winner}
-        setRendering={setRendering}
-        rendering={rendering}
-      />
-      {accessToken === null ? (
-        <CommentList accessToken={accessToken} id={id} rendering={rendering} />
-      ) : (
-        <CommentList accessToken={accessToken} id={id} rendering={rendering} />
-      )}
-    </>
-  );
+	return (
+		<>
+			<GameButtons isReload={true} id={id} />
+			<Comment
+				winner={winner}
+				setRendering={setRendering}
+				rendering={rendering}
+			/>
+			{accessToken === null ? (
+				<CommentList accessToken={accessToken} id={id} rendering={rendering} />
+			) : (
+				<CommentList accessToken={accessToken} id={id} rendering={rendering} />
+			)}
+		</>
+	);
 }
