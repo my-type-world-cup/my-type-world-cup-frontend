c39f3f2f594652f23bc6e8dc45d1357fa3861eef
nakjun <111031253+nakjun12@users.noreply.github.com>
2023-08-18 08:27:09 +0900

    restyle : 404, callback 문구 수정

--- my-type-world-cup/src/pages/404.tsx
+++ my-type-world-cup/src/pages/404.tsx
@@ -3,27 +3,32 @@ import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 export default function Custom404() {
-  const router = useRouter();
+	const router = useRouter();
 
-  useEffect(() => {
-    // 2초 후에 "/" 경로로 리다이렉트
-    const timeout = setTimeout(() => {
-      router.push("/");
-    }, 2000);
+	useEffect(() => {
+		// 2초 후에 "/" 경로로 리다이렉트
+		const timeout = setTimeout(() => {
+			router.push("/");
+		}, 2000);
 
-    // cleanup 함수에서 timeout clear
-    return () => clearTimeout(timeout);
-  }, [router]);
+		// cleanup 함수에서 timeout clear
+		return () => clearTimeout(timeout);
+	}, [router]);
 
-  return (
-    <div className="h-screen ">
-      <div className="mt-20">
-        <h1>404 - Page Not Found</h1>
-        <p>The page you are looking for cannot be found.</p>
-        <p>
-          <Link href="/">goback</Link>
-        </p>
-      </div>
-    </div>
-  );
+	return (
+		<div className="h-screen flex flex-col justify-center items-center">
+			<div className="text-center">
+				<h1 className="text-4xl mb-4">404 - Page Not Found</h1>
+				<p className="mb-2">페이지를 찾을 수 없습니다.</p>
+				<p className="mb-2">2초 후에 홈으로 돌아갑니다.</p>
+				<p>
+					<Link href="/">
+						<a className="text-blue-500 hover:underline">
+						이 곳을 누르면 바로 넘어갑니다.
+						</a>
+					</Link>
+				</p>
+			</div>
+		</div>
+	);
 }
--- my-type-world-cup/src/pages/callback.tsx
+++ my-type-world-cup/src/pages/callback.tsx
@@ -3,8 +3,8 @@ import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { useRecoilState, useSetRecoilState } from "recoil";
 import { accessTokenState, lastPath, userState } from "../lib/atom/atom";
-const dummy =
-  "eyJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6WyJST0xFX1VTRVIiXSwidXNlcm5hbWUiOiJ3bnM0NTBAZ21haWwuY29tIiwic3ViIjoid25zNDUwQGdtYWlsLmNvbSIsImlhdCI6MTY4OTM4MDAwMCwiZXhwIjoxNjg5MDAwMDAwfQ.Q-6tO2FQ6rQ8IXZD8jm6f7AN45fG_qpj0zqjD4-C2Yc";
+// const dummy =
+//   "eyJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6WyJST0xFX1VTRVIiXSwidXNlcm5hbWUiOiJ3bnM0NTBAZ21haWwuY29tIiwic3ViIjoid25zNDUwQGdtYWlsLmNvbSIsImlhdCI6MTY4OTM4MDAwMCwiZXhwIjoxNjg5MDAwMDAwfQ.Q-6tO2FQ6rQ8IXZD8jm6f7AN45fG_qpj0zqjD4-C2Yc";
 
 type Props = {};
 
@@ -25,30 +25,19 @@ export default function Callback({}: Props) {
           }
         })
         .catch((error) => {
-          // if (error === 401) {
-          //   post_refresh()
-          //     .then((data: any) => {
-          //       setAccessToken(data.data);
-          //       console.log(data.data);
-          //     })
-          //     .catch((error) => {
-          //       console.error(error);
-          //     });
-          //   console.log("로그인 해야해~");
-          // }
-
           console.error(error);
         });
 
-      console.log("1");
     } else if (router.query.access_token) {
-      console.log("2");
       setAccessToken(router.query.access_token as string); // 토큰 저장
     } else if (!router.query.access_token && !lastPathState) {
-      console.log("3");
       router.replace("/"); // 토큰이 없고 이전 경로가 없으면 메인으로 이동
     }
   }, [router.query.access_token, accessToken, setAccessToken]);
 
-  return <div className="mt-40">callback</div>;
+  return (
+		<div className="mt-40 flex justify-center items-center">
+			<p>Loading...</p>
+		</div>
+	);
 }
