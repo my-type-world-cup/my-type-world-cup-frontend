0f1d6a783317ebc62e776a6462bdc138128027ee
nakjun <111031253+nakjun12@users.noreply.github.com>
2023-09-06 14:32:52 +0900

    fix : Ga4 plan b

--- my-type-world-cup/src/pages/_app.tsx
+++ my-type-world-cup/src/pages/_app.tsx
@@ -3,12 +3,9 @@ import Header from "@/components/all/Header";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import { useRouter } from 'next/router';
-import Script from 'next/script';
-import { useEffect } from 'react';
 import { RecoilRoot } from "recoil";
 import { SWRConfig } from "swr";
-import * as gtag from '../lib/gtag';
+import GoogleAnalytics from "../lib/GoogleAnalytics"
 
 const swrConfig = {
 	revalidateOnFocus: false,
@@ -16,24 +13,7 @@ const swrConfig = {
 };
 
 function App({ Component, pageProps }: AppProps) {
-	  const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url: URL) => {
-      gtag.pageview(url);
-    };
-    router.events.on('routeChangeComplete', handleRouteChange);
-    router.events.on('hashChangeComplete', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-      router.events.off('hashChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
-
-
-
-
-  return (
+	return (
 		<div className="bg-hover flex justify-center items-center mt-[-1px] w-auto overflow-hidden">
 			<Head>
 				<title>이상형 월드컵</title>
@@ -41,24 +21,7 @@ function App({ Component, pageProps }: AppProps) {
 				<link rel="mask-icon" href="/icon/trophy.svg" color="#000000" />
 				<MetaTags />
 			</Head>
-			  <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${gtag.GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
+			 <GoogleAnalytics />
 			<div className="my-auto h-0 lg:h-auto mt-40 mr-4 hidden lg:block">
 				<p className="mt-4 text-2xl text-left">나의 마음을 확인하세요</p>
 				<h2 className="text-left text-5xl font-bold text-[#117FFA]">
--- my-type-world-cup/src/pages/_document.tsx
+++ my-type-world-cup/src/pages/_document.tsx
@@ -1,13 +1,26 @@
+//pages/document.tsx
+
 import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html>
       <Head />
-
       <body>
         <Main />
         <NextScript />
+
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        {/* Necessary to prevent error: window.gtag is not defined for Next.js-hydration */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          `,
+          }}
+        />
       </body>
     </Html>
   );
