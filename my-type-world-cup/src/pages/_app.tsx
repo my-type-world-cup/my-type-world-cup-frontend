71d09b4bad715a8420c5cb8e848eb24519b42dc4
nakjun <111031253+nakjun12@users.noreply.github.com>
2023-09-06 15:30:37 +0900

    remove : ga4 컴포넌트 위치 수정

--- my-type-world-cup/src/pages/_app.tsx
+++ my-type-world-cup/src/pages/_app.tsx
@@ -5,7 +5,7 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import { RecoilRoot } from "recoil";
 import { SWRConfig } from "swr";
-import GoogleAnalytics from "../lib/GoogleAnalytics"
+import GoogleAnalytics from "../lib/GoogleAnalytics";
 
 const swrConfig = {
 	revalidateOnFocus: false,
@@ -20,8 +20,8 @@ function App({ Component, pageProps }: AppProps) {
 				<link rel="icon" href="/icon/trophy.svg" />
 				<link rel="mask-icon" href="/icon/trophy.svg" color="#000000" />
 				<MetaTags />
+				<GoogleAnalytics />
 			</Head>
-			 <GoogleAnalytics />
 			<div className="my-auto h-0 lg:h-auto mt-40 mr-4 hidden lg:block">
 				<p className="mt-4 text-2xl text-left">나의 마음을 확인하세요</p>
 				<h2 className="text-left text-5xl font-bold text-[#117FFA]">
