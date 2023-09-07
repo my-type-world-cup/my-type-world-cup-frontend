5f156b592effccfb2789015d0f53d5f31d30936f
nakjun12 <wns450@gmail.com>
2023-09-08 00:22:55 +0900

    remove :  conflict 수정

--- my-type-world-cup/src/pages/_document.tsx
+++ my-type-world-cup/src/pages/_document.tsx
@@ -3,25 +3,13 @@
 import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
-  return (
-    <Html>
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-
-        {/* Global Site Tag (gtag.js) - Google Analytics */}
-        {/* Necessary to prevent error: window.gtag is not defined for Next.js-hydration */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-          `,
-          }}
-        />
-      </body>
-    </Html>
-  );
+	return (
+		<Html>
+			<Head />
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
