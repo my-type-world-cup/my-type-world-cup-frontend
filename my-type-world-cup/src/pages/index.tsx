b1b92cfb2e5fd03d9d4b1539f65330570363bf01
nakjun12 <wns450@gmail.com>
2023-09-13 00:28:22 +0900

    remove : ssr 삭제
    swr infinite로 인한 삭제
    부드럽게 연동되지않음
    올바르게 작동하려면 swr로 수정해야하는데,
    시간이 많이 소모되기때문에 패스하였음
    추후에는 이 조합을 활용할 예정

--- my-type-world-cup/src/pages/index.tsx
+++ my-type-world-cup/src/pages/index.tsx
@@ -1,15 +1,8 @@
-import { fetcher } from "@/api/swr_fetch";
 import SearchBar from "@/components/main/SearchBar";
 import SortButtons from "@/components/main/SortButtons";
 import WorldcupList from "@/components/main/WorldcupList";
-import { BACK_URL } from "@/lib/config";
 import useWorldcupsStateWithSWR from "@/lib/hooks/useWorldcupsStateWithSWR";
-import type { WorldcupsResponse } from "@/type/Types";
 import { Sort_buttons } from "@/type/Types";
-
-type HomeProps = {
-	initialData: WorldcupsResponse;
-};
 
 export const sortButtons: Sort_buttons[] = [
 	{ name: "인기순", value: "playCount" },
@@ -17,9 +10,9 @@ export const sortButtons: Sort_buttons[] = [
 	{ name: "댓글순", value: "commentCount" }
 ];
 
-export default function Home({ initialData }: HomeProps) {
+export default function Home() {
 	const { containerRef, sort, setSort, setSearch, worldcups, isLoading } =
-		useWorldcupsStateWithSWR("/worldcups", initialData);
+		useWorldcupsStateWithSWR("/worldcups");
 
 	return (
 		<main
@@ -31,20 +24,3 @@ export default function Home({ initialData }: HomeProps) {
 		</main>
 	);
 }
-
-export const getServerSideProps = async () => {
-	const sort = "playCount"; // 정렬 방식
-	const page = 1; // 초기 페이지 번호
-	const size = 10; // 페이지당 데이터 수
-
-	const url = `${BACK_URL}/worldcups?page=${page}&size=${size}&sort=${sort}`;
-
-	// 서버에서 초기 데이터를 불러옵니다.
-	const initialData = await fetcher(url);
-
-	return {
-		props: {
-			initialData
-		}
-	};
-};
