e8f41b4fa62c4b4feecf85f305338ffd08ee2c6c
nakjun12 <wns450@gmail.com>
2023-09-26 00:18:38 +0900

    remove : 컴포넌트 분리로 인한 함수 이동

--- my-type-world-cup/src/components/editor/CandidateManagementPanel/CandidateManagement.tsx
+++ my-type-world-cup/src/components/editor/CandidateManagementPanel/CandidateManagement.tsx
@@ -1,8 +1,8 @@
 import { useCandidateManagementState } from "@/lib/hooks/useCandidateManagementState";
 import type { Editor_step, Post_res } from "@/type/Types";
-import { ChangeEvent, Dispatch, SetStateAction, useEffect } from "react";
-import CandidateList from "./CandidateList";
-import ImageUpload from "./ImageUpload";
+import { Dispatch, SetStateAction, useEffect } from "react";
+import ImageUpload from "./imageupload/ImageUpload";
+import CandidateList from "./candidateList/CandidateList";
 type Props = {
   saveWorldcup: Post_res | null;
   setIsNumber: Dispatch<SetStateAction<Editor_step>>;
@@ -35,23 +35,6 @@ export default function CandidateManagement({
       setIsNumber(1);
     }
   }, []);
-
-  //파일을 선택했을 때 호출되는 비동기 함수
-  async function onSelectFile(e: ChangeEvent<HTMLInputElement>) {
-    if (!e.target.files || e.target.files.length === 0) return;
-
-    const file = e.target.files[0];
-    const reader = new FileReader();
-
-    // 이벤트 리스너를 먼저 등록한 후에 readAsDataURL 함수를 호출
-    const fileReadResult = await new Promise<string>((resolve) => {
-      reader.onload = () => resolve(reader.result?.toString() || "");
-      reader.onerror = () => resolve("");
-      reader.readAsDataURL(file);
-    });
-
-    setImgSrc(fileReadResult);
-  }
 
   if (!saveWorldcup) {
     return <></>;
